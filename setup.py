377b1a9f8365634784ed3f9a21cb8b8b721a9ef4
lg <lg4869@outlook.com>
2018-10-11 19:51:38 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+
+
+def kungfu_tensorflow_lib():
+    import tensorflow as tf
+    return Extension(
+        'kungfu_tensorflow_lib',
+        sources=[
+            'src/negotiator.cpp',
+        ],
+        include_dirs=[
+            tf.sysconfig.get_include(),
+        ],
+        libraries=[
+            'tensorflow_framework',
+        ],
+        library_dirs=[
+            tf.sysconfig.get_lib(),
+        ],
+        extra_compile_args=[
+            '-std=c++11',
+        ],
+    )
+
+
+setup(
+    name='kungfu',
+    version='0.0.0',
+    packages=find_packages(),
+    description='The ultimate distributed training framework for TensorFlow',
+    url='https://github.com/luomai/kungfu',
+    ext_modules=[
+        kungfu_tensorflow_lib(),
+    ],
+    setup_requires=[],
+    install_requires=[],
+)
