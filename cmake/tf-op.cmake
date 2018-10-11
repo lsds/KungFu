8b09805cfed5f6660ddc696842156988b5b695a3
lg <lg4869@outlook.com>
2018-10-11 23:01:30 +0800

    make cmake target name to the same as python ext

--- cmake/tf-op.cmake
+++ cmake/tf-op.cmake
@@ -19,10 +19,25 @@ EXECUTE_PROCESS(COMMAND
                 '\n'
                 OUTPUT_VARIABLE
                 TF_LIB)
+
+EXECUTE_PROCESS(
+    COMMAND
+    python3
+    -c
+    "import sysconfig; print(sysconfig.get_config_var('EXT_SUFFIX'))"
+    COMMAND
+    tr
+    -d
+    '\n'
+    OUTPUT_VARIABLE
+    PY_EXT_SUFFIX)
+
 LINK_DIRECTORIES(${TF_LIB})
 
 FUNCTION(ADD_TF_OP_LIB target)
     ADD_LIBRARY(${target} SHARED ${ARGN})
     TARGET_LINK_LIBRARIES(${target} tensorflow_framework)
     TARGET_INCLUDE_DIRECTORIES(${target} PRIVATE ${TF_INCLUDE})
+    SET_TARGET_PROPERTIES(${target} PROPERTIES SUFFIX ${PY_EXT_SUFFIX})
+    SET_TARGET_PROPERTIES(${target} PROPERTIES PREFIX "")
 ENDFUNCTION()
