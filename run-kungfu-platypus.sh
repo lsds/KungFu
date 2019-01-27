e15c9fb1a5d3b6bca97ab9f718c77406e6463d35
Andrei-Octavian Brabete <ab7515@ic.ac.uk>
2019-01-27 10:31:47 +0000

    Run kungfu on platypus script

---
+++ run-kungfu-platypus.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+cd $HOME/kungfu
+
+build(){
+	git clean -fdx
+	pip3 install --no-index --user -U .
+	./scripts/go-install.sh --no-tests
+}
+
+#build
+
+#python3 benchmarks/tensorflow_synthetic_benchmark.py
+
+np=4
+H=127.0.0.1:4
+ALGO=SIMPLE
+
+./bin/kungfu-prun \
+    -np $np \
+    -H $H \
+    -timeout 120s \
+    -algo $ALGO \
+    python3 \
+    benchmarks/tensorflow_synthetic_benchmark.py
