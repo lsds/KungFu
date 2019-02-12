960b57df71d45d6a04b4a256083a92b892e64568
lg <lg4869@outlook.com>
2019-02-12 20:11:33 +0800

    paralellel train

--- experiments/kungfu/atari_pingpong/pingpong.py
+++ experiments/kungfu/atari_pingpong/pingpong.py
@@ -18,6 +18,13 @@ def parse_args():
         '--batch-size', type=int, default=10, help='batch size')
     parser.add_argument(
         '--episodes', type=int, default=50000, help='max number of episodes')
+    parser.add_argument(
+        '--checkpoint',
+        type=str,
+        default='pingpong.npz',
+        help='path to checkpoint file')
+    parser.add_argument(
+        '--init', type=bool, default=False, help='generate init checkpoint')
     return parser.parse_args()
 
 
@@ -40,10 +47,6 @@ def init_agent(env, agent):
         from mlp_agent import Agent
 
     a = Agent(image.shape, env.action_space)
-    a.save('pingpong-init.npz')
-    checkpoing = 'pingpong.latest.npz'
-    if os.path.isfile(checkpoing):
-        a.load(checkpoing)
     return a
 
 
@@ -51,6 +54,14 @@ def main():
     args = parse_args()
     env = gym.make("Pong-v0")
     agent = init_agent(env, args.agent)
+    if args.init:
+        print('saving checkpoint %s' % args.checkpoint)
+        agent.save(args.checkpoint)
+        return
+    else:
+        print('loading checkpoint %s' % args.checkpoint)
+        agent.load(args.checkpoint)
+
     trainer = DiffTrainer(args.batch_size, prepro)
     trainer.train(agent, env, args.episodes)
 
--- experiments/kungfu/atari_pingpong/run.sh
+++ experiments/kungfu/atari_pingpong/run.sh
@@ -2,9 +2,26 @@
 set -e
 
 cd $(dirname $0)
+ROOT=$(cd ../../.. && pwd)
+
+KUNGFU_PRUN=${ROOT}/bin/kungfu-prun
+if [ ! -f ${KUNGFU_PRUN} ]; then
+    ${ROOT}/scripts/go-install.sh
+fi
 
 if [ $(uname -s) = "Darwin" ]; then
     export DYLD_LIBRARY_PATH=$(python3 -c "import os; import kungfu; print(os.path.dirname(kungfu.__file__))")
 fi
 
-python3 ./pingpong.py
+python3 ./pingpong.py --init=1
+
+np=8
+batch_size=1
+timeout=1m
+
+${KUNGFU_PRUN} \
+    -timeout $timeout \
+    -np $np \
+    python3 \
+    ./pingpong.py \
+    --batch-size $batch_size
