0b16094e6d1bdd31c2ba2470390f3f26525d092c
lg <lg4869@outlook.com>
2019-02-16 23:03:53 +0800

    increase timeout

--- scripts/tests/run-train-tests.sh
+++ scripts/tests/run-train-tests.sh
@@ -37,7 +37,7 @@ run_parallel_train_test() {
     local np=$2
 
     local hosts=127.0.0.1:$np
-    local timeout=20s
+    local timeout=$((np * 6))s
     local batch_size=$((total_batch_size / np))
 
     ${KUNGFU_PRUN} \
