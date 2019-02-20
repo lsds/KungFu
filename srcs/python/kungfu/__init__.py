ec79e7a694c645e11cbbd5f4d24d2dbd7374751d
lg <lg4869@outlook.com>
2019-02-20 22:41:48 +0800

    use tf.group

--- srcs/python/kungfu/__init__.py
+++ srcs/python/kungfu/__init__.py
@@ -10,4 +10,4 @@ def distributed_variables_initializer():
     # with tf.control_dependencies([tf.global_variables_initializer()]):
     for v in g.get_collection(tf.GraphKeys.GLOBAL_VARIABLES):
         ops.append(tf.assign(v, _op_lib.broadcast(v)))
-    return ops
+    return tf.group(ops)
