fc4700bc85186dc0511ff481ed6e4b45886a8692
Luo Mai <luo.mai.cs@gmail.com>
2019-02-22 15:03:23 +0000

    update names

--- srcs/go/monitor/metrics.go
+++ srcs/go/monitor/metrics.go
@@ -10,8 +10,8 @@ import (
 )
 
 type NetMetrics struct {
-	sent *rateAccumulator
-	recv *rateAccumulator
+	egress  *rateAccumulator
+	ingress *rateAccumulator
 }
 
 var netMetrics *NetMetrics
@@ -26,8 +26,8 @@ func GetNetMetrics() *NetMetrics {
 
 func newNetMetrics(p time.Duration) *NetMetrics {
 	m := &NetMetrics{
-		sent: newRateAccumulator("send"),
-		recv: newRateAccumulator("recv"),
+		egress:  newRateAccumulator("Egress rate in bytes"),
+		ingress: newRateAccumulator("Ingress rate in bytes"),
 	}
 	if p > 0 {
 		go m.start(p)
@@ -37,24 +37,24 @@ func newNetMetrics(p time.Duration) *NetMetrics {
 
 func (m *NetMetrics) start(p time.Duration) {
 	for range time.Tick(p) {
-		m.sent.r.update()
-		m.recv.r.update()
+		m.egress.r.update()
+		m.ingress.r.update()
 	}
 }
 
 func (m *NetMetrics) Egress(n int64, a plan.Addr) {
 	// TODO: add labels from Addr
-	m.sent.a.Add(n)
+	m.egress.a.Add(n)
 }
 
 func (m *NetMetrics) Ingress(n int64, a plan.Addr) {
 	// TODO: add labels from Addr
-	m.recv.a.Add(n)
+	m.ingress.a.Add(n)
 }
 
 func (m *NetMetrics) WriteTo(w io.Writer) {
-	m.sent.WriteTo(w)
-	m.recv.WriteTo(w)
+	m.egress.WriteTo(w)
+	m.ingress.WriteTo(w)
 }
 
 func (m *NetMetrics) ServeHTTP(w http.ResponseWriter, req *http.Request) {
