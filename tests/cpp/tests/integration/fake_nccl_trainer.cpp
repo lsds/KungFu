328d8cbe40d9c90bf2e2cd4fe0d3bd475d537caf
lg <lg4869@outlook.com>
2019-02-25 16:17:15 +0800

    test various sizes

--- tests/cpp/tests/integration/fake_nccl_trainer.cpp
+++ tests/cpp/tests/integration/fake_nccl_trainer.cpp
@@ -22,9 +22,20 @@ template <typename T> struct fake_gpu_buffer_t {
         : name(name), count(count), send_buf(count), recv_buf(count)
     {
         printf("fake_gpu_buffer_t of count %d, size %d\n", count,
-               count * sizeof(T));
+               (int)(count * sizeof(T)));
     }
 };
+
+constexpr size_t Mi = 1 << 10;
+
+void simple_test(int size)
+{
+    printf("simple_test of size: %d Mi\n", (int)(size / Mi));
+    int n = size / sizeof(float);
+    cuda_vector<float> x(n);
+    cuda_vector<float> y(n);
+    nccl.all_reduce(x.data(), y.data(), n, "test-tensor");
+}
 
 template <typename Collective> int main1(int argc, char *argv[])
 {
@@ -36,13 +47,9 @@ template <typename Collective> int main1(int argc, char *argv[])
 
     nccl_collective nccl(id, bootstrap.cluster_size(), bootstrap.rank());
 
-    {
-        int n = 10;
-        cuda_vector<float> x(n);
-        cuda_vector<float> y(n);
-        nccl.all_reduce(x.data(), y.data(), n, "test-tensor");
-    }
-    printf("simple test is done\n");
+    for (int i = 1; i < 10; ++i) { simple_test(i * Mi); }
+    for (int i = 1; i < 10; ++i) { simple_test(i * 10 * Mi); }
+    printf("simple tests are done\n");
 
     const auto grad_sizes = resnet50_grad_sizes();
     run_experiment<nccl_collective, fake_gpu_buffer_t<float>>(grad_sizes, nccl);
