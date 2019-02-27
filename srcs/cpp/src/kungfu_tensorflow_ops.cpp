6da74e2e33a7df6be4a1b3542f45d3058f532c90
Andrei-Octavian Brabete <brabete.andrei96@gmail.com>
2019-02-27 17:38:20 +0000

    New ako implementation with ingress and egress gradients. This variant is built on top of the allreduce collective communication, following to build point to point communication for ako algorithm.

--- srcs/cpp/src/kungfu_tensorflow_ops.cpp
+++ srcs/cpp/src/kungfu_tensorflow_ops.cpp
@@ -4,6 +4,8 @@
 #include <kungfu_base.h>
 #include <kungfu_tensorflow_ops.h>
 #include <queue>
+#include <mutex>
+#include <atomic>
 
 #include <chrono>
 
@@ -50,36 +52,36 @@ class AllReduce : public AsyncOpKernel
 
 REGISTER_KERNEL_BUILDER(Name("AllReduce").Device(DEVICE_CPU), AllReduce);
 
-typedef std::function<void(Tensor&, Tensor&)> AccumulateExternalGradientsCallback;
-
-void accumulateExternalFunc(Tensor& accumulatedLocalGradients, Tensor& gradients) {
-
-}
-
-
 // Ako implementation
-class AkoNegotiator : public OpKernel
+class AkoNegotiator : public AsyncOpKernel
 {
-    using OpKernel::OpKernel;
+    using AsyncOpKernel::AsyncOpKernel;
 
   public:
     // have them public for now
 
-    std::queue<Tensor> localGradientsWindow;
-    Tensor accumulatedLocalGradients;
-    Tensor accumulatedExternalGradients;
-
-     explicit OpKernel(OpKernelConstruction* context) {
-        // accummulateExternalCb =    
-     }
+    std::queue<Tensor> tensorWindow;
+    Tensor outGrad; // the accumulated gradient to be negotiated
+    Tensor inGrad;  // the accumulated gradient received through negotiation
+    std::mutex inGradMutex;
+    std::atomic<bool> hasInGrad;
 
 
-    void Compute(OpKernelContext *context) override
+    explicit AkoNegotiator(OpKernelConstruction* context) : AsyncOpKernel(context) {
+        std::cout << "Initializing ako negotiator" << std::endl;
+        hasInGrad = true;
+        std::cout << "After init ako negotiator" << std::endl;
+        
+    }
+
+    void ComputeAsync(OpKernelContext *context, DoneCallback done) override
     {
+        // Check arg count: gradient tensors group, number partitions, current
+        // partition index
         DCHECK_EQ(4, context->num_inputs());
 
         
-        const Tensor &gradients          = context->input(0);    
+        Tensor &gradients             = (Tensor&) context->input(0);    
 
         const Tensor &currentPartitionIndex = context->input(1);
         const Tensor &pAkoPartitions        = context->input(2);
@@ -92,56 +94,100 @@ class AkoNegotiator : public OpKernel
         auto numberPartitionsTensor      = pAkoPartitions.vec<int>();
         auto kickinTimeTensor            = kickinTime.vec<int>();
 
-        int numberPartitions = numberPartitionsTensor(0);
-        int partitionIndex   = currentPartitionIndexTensor(0);
-        int kickin           = kickinTimeTensor(0);
+        uint32_t numberPartitions = numberPartitionsTensor(0);
+        uint32_t partitionIndex   = currentPartitionIndexTensor(0);
+        uint32_t kickin           = kickinTimeTensor(0);
 
         OP_REQUIRES_OK(context,
                        context->allocate_output(0, gradients.shape(), &output));
 
+        // Update gradient window
 
-        localGradientsWindow.push(gradients);    
-
+        tensorWindow.push(gradients);    
         Tensor stale;
-        if(localGradientsWindow.size() > numberPartitions) {
-            stale = localGradientsWindow.front();
-            localGradientsWindow.pop();
+        if(tensorWindow.size() > numberPartitions) {
+            stale = tensorWindow.front();
+            tensorWindow.pop(); 
         }
-    
-        if (_kungfu_world.GetGlobalStep() % numberPartitions == partitionIndex) {           
-            if(accumulatedLocalGradients.NumElements() == 0) {
-                Tensor grad_accumulated(DataTypeToEnum<float>::v(), gradients.shape());
-                auto grad_accumulated_flt = grad_accumulated.flat<float>();
-                for (int i = 0; i < grad_accumulated_flt.size(); ++i) {
-                    grad_accumulated_flt(i) = 0.0;
-                }
-               accumulatedLocalGradients = grad_accumulated;
-            }
 
-            auto grads_flt = gradients.flat<float>();
-            auto accumulatedLocalGradients_flt = accumulatedLocalGradients.flat<float>();
+        if(outGrad.NumElements() == 0) {
+            Tensor tensorReset(DataTypeToEnum<float>::v(), gradients.shape());
+            Tensor grad_accumulated = tensorReset;
+            auto grad_accumulated_flt = grad_accumulated.flat<float>();
+            for (int i = 0; i < grad_accumulated_flt.size(); ++i) {
+                grad_accumulated_flt(i) = 0.0;
+            }
+            outGrad = grad_accumulated;
+        }
+
+
+
+        auto grads_flt   = gradients.flat<float>();
+        auto outGrad_flt = outGrad.flat<float>();
+        auto inGrad_flt  = inGrad.flat<float>();
+        auto stale_flt   = stale.flat<float>();
+
+        std::cout << "XXXXX" << std::endl;
+
+        bool t = true;
+        bool f = false;
+        // TODO the bug is here. check if a tensor is empty
+        // compare exc does not work, 0 shape 
+        if (hasInGrad.compare_exchange_strong(t, f)) {
+            std::lock_guard<std::mutex> lock(inGradMutex);
             
-            auto expire = stale.NumElements() > 0;
-            for (int i = 0; i < accumulatedLocalGradients_flt.size(); ++i) {
-                auto expiredTensor = expire ? stale.flat<float>()(i) : 0.0;
-                accumulatedLocalGradients_flt(i) = grads_flt(i) + accumulatedLocalGradients_flt(i) - expiredTensor;
-            }
+            std::cout << inGrad.DebugString() << std::endl;
 
-            _kungfu_world.AllReduce(accumulatedLocalGradients.tensor_data().data(),
-                                    (void *)(output->tensor_data().data()),
-                                    accumulatedLocalGradients.NumElements(),
-                                    to_kungfu_type(accumulatedLocalGradients.dtype()), KungFu_SUM,
-                                    name().c_str(), accumulateExternalCb); // give it an empty callback to make it async
-        } else {
-            auto flt = output->flat<float>();
-            for (int i = 0; i < flt.size(); ++i) {
-                flt(i) = 0.0;
+            for (int i = 0; i < grads_flt.size(); ++i) {
+                 grads_flt(i) = grads_flt(i) + inGrad_flt(i);
             }
+            Tensor tensorReset(DataTypeToEnum<float>::v(), gradients.shape());
+            inGrad = tensorReset;
+        }
+
+        std::cout << "YYYYY" << std::endl;
+
+
+        auto expire = stale.NumElements() > 0;
+        // TODO: cast operations to pointers to avoid bound checks
+        for (int i = 0; i < outGrad_flt.size(); ++i) {
+            auto stale = expire ? stale_flt(i) : 0.0;
+            outGrad_flt(i) = grads_flt(i) + outGrad_flt(i) - stale;
+        }
+
+        if (_kungfu_world.GetGlobalStep() % numberPartitions == partitionIndex) {           
+            std::function<void()> func = [&]() {
+                std::lock_guard<std::mutex> lock(inGradMutex);
+                hasInGrad = true;
+                std::cout << "I am calling the callback" << std::endl;
+                
+                // subract gradients from inGrad to not apply them twice
+                for (int i = 0; i < inGrad_flt.size(); ++i) {
+                     inGrad_flt(i) = inGrad_flt(i) - grads_flt(i);
+                }
+                done();
+            };
+            CallbackWrapper accumulatedGradientCallback(func);
+             
+
+             std::cout << "The pointer is " << (&inGrad) << std::endl;
+            _kungfu_world.AllReduce(outGrad.tensor_data().data(),
+                                    (void *)((&inGrad)->tensor_data().data()),
+                                    outGrad.NumElements(),
+                                    to_kungfu_type(outGrad.dtype()), KungFu_SUM,
+                                    name().c_str(), accumulatedGradientCallback);
+            // The name will not be unique in the async case because it would not be 
+            // able to differentiated the traffic of the allreduce with diff global steps
+            std::cout << "Before 1" << std::endl;
+            *output = gradients;
+            std::cout << "After 1" << std::endl;
+        } else {
+            std::cout << "Before 2" << std::endl;
+            *output = gradients;
+            std::cout << "After 2" << std::endl;
+            done();
         }
     }
-
-    private:
-       AccumulateExternalGradientsCallback accummulateExternalCb;
 };
 
 REGISTER_KERNEL_BUILDER(Name("AkoNegotiator").Device(DEVICE_CPU),
