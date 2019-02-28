25efee19d26beeb4451a0fd0783f1e1a9bd64881
lg <lg4869@outlook.com>
2019-02-28 19:02:18 +0800

    new cmake helper function :: ADD_CGO_DEPS

--- cmake/cgo.cmake
+++ cmake/cgo.cmake
@@ -1,7 +1,10 @@
+SET(CGO_OUTPUT_DIRECTORY ${LIBRARY_OUTPUT_PATH}
+    CACHE STRING "directory for CGO output")
+
 FUNCTION(ADD_CGO_LIBRARY target)
     FILE(MAKE_DIRECTORY ${LIBRARY_OUTPUT_PATH})
     ADD_CUSTOM_TARGET(${target} ALL
-                      WORKING_DIRECTORY ${LIBRARY_OUTPUT_PATH}
+                      WORKING_DIRECTORY ${CGO_OUTPUT_DIRECTORY}
                       COMMAND env #
                               CGO_CFLAGS=${CGO_CFLAGS}
                               CGO_LDFLAGS=${CGO_LDFLAGS}
@@ -17,7 +20,7 @@ FUNCTION(ADD_CGO_SHARED_LIBRARY target)
     GET_FILENAME_COMPONENT(NAME ${ARGN} NAME)
     SET(SO_NAME "${NAME}${CMAKE_SHARED_LIBRARY_SUFFIX}")
     ADD_CUSTOM_TARGET(${target} ALL
-                      WORKING_DIRECTORY ${LIBRARY_OUTPUT_PATH}
+                      WORKING_DIRECTORY ${CGO_OUTPUT_DIRECTORY}
                       COMMAND env #
                               CGO_CFLAGS=${CGO_CFLAGS}
                               CGO_LDFLAGS=${CGO_LDFLAGS}
@@ -28,4 +31,15 @@ FUNCTION(ADD_CGO_SHARED_LIBRARY target)
                               -o
                               ${SO_NAME}
                               ${ARGN})
+ENDFUNCTION()
+
+FUNCTION(ADD_CGO_DEPS target)
+    TARGET_INCLUDE_DIRECTORIES(${target} PRIVATE ${CGO_OUTPUT_DIRECTORY})
+    # Some CGO libraries require extra dependencies
+    IF(APPLE)
+        TARGET_LINK_LIBRARIES(${target} "-framework CoreFoundation")
+        TARGET_LINK_LIBRARIES(${target} "-framework Security")
+    ELSE()
+        TARGET_LINK_LIBRARIES(${target} rt)
+    ENDIF()
 ENDFUNCTION()
