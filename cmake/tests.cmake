6c523603541767560ab23822a181a73a11db0adf
Luo Mai <luo.mai.cs@gmail.com>
2019-03-01 16:59:25 +0000

    improve CMAKE

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -1,5 +1,7 @@
 OPTION(KUNGFU_BUILD_GTEST "Build gtest from source." OFF)
 
+FIND_PACKAGE(Threads REQUIRED)
+
 FUNCTION(LINK_KUNGFU_LIBS target)
     TARGET_LINK_LIBRARIES(${target} kungfu-base kungfu-comm kungfu)
 ENDFUNCTION()
