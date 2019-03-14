23a2e1d180b8e3f3dc00e3d8da35d1d98aa6b4ac
lg <lg4869@outlook.com>
2019-03-15 00:58:28 +0800

    add a script for manual run nccl test

---
+++ scripts/tests/run-nccl-train-test.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+cd $(dirname $0)
+SCRIPT_DIR=$(pwd)
+SCRIPT_NAME=$(dirname $0)
+cd ../..
+ROOT=$(pwd)
+
+export NCCL_HOME=$HOME/local/nccl
+
+env \
+    KUNGFU_USE_NCCL=1 \
+    pip3 install --no-index --user -U .
+
+KUNGFU_PRUN=${ROOT}/bin/kungfu-prun
+if [ ! -f ${KUNGFU_PRUN} ]; then
+    ${ROOT}/scripts/go-install.sh
+fi
+
+SCRIPT=${ROOT}/experiments/kungfu/kf_tensorflow_synthetic_benchmark.py
+
+# FIXME: don't depend on LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$NCCL_HOME/lib
+
+np=4
+timeout=45s
+
+${KUNGFU_PRUN} \
+    -timeout $timeout \
+    -np $np \
+    python3 \
+    ${SCRIPT}
