a0a3f8a768a6bad6a86b6aa30e7b6accc93fd84f
Andrei-Octavian Brabete <brabete.andrei96@gmail.com>
2019-05-14 17:55:16 +0100

    Dynamic partitioning

--- srcs/python/kungfu/ops.py
+++ srcs/python/kungfu/ops.py
@@ -17,6 +17,7 @@ def get_num_peers():
     num_peers = len(cluster_spec['Peers'])
     return num_peers
 
+
 def _load_op_lib(name):
     module_path = os.path.dirname(__file__)
     suffix = sysconfig.get_config_var(EXT_SUFFIX_KEY)
@@ -48,6 +49,7 @@ def _load_and_init_op_lib():
 
 _op_lib, _has_gpu = _load_and_init_op_lib()
 
+
 def broadcast(t):
     return _op_lib.broadcast(t)
 
@@ -55,32 +57,14 @@ def broadcast(t):
 def all_reduce(t):
     return _op_lib.all_reduce(t, input_tensor_name=t.name[:-2])
 
-# Based on Andrei - Octavian Brabete
-def partial_exchange_all_reduce(t, budget, count_gradients, accumulate, average, fraction, find_epoch_denominator):
-    # Take full gradient name for unicity
-    tensor_size = t.shape.num_elements() * t.dtype.size
-    if accumulate:
-        import json, os
-        cluster_spec = json.loads(os.getenv('KUNGFU_CLUSTER_SPEC'))
-        num_peers = len(cluster_spec['Peers'])
-        return _op_lib.partial_accumulating_negotiator(t, input_tensor_name=t.name, budget=budget, 
-                                                       tensor_size=tensor_size, count_gradients=count_gradients, num_peers=num_peers, average=average)
-    else:
-        return _op_lib.partial_negotiator(t, input_tensor_name=t.name, budget=budget, 
-                                          tensor_size=tensor_size, count_gradients=count_gradients, 
-                                          find_epoch_denominator=find_epoch_denominator, fraction=fraction)
-
-# Based on Guo Li
-def partial_exchange_all_reduce_front_end_partitioning(t, index, partitions, accumulate=False, average=False):
-    # Take full gradient name for unicity
-    tensor_size = t.shape.num_elements() * t.dtype.size
-    return _op_lib.partial_negotiator_front_end_partitioning(t, index=index, partitions=partitions)
 
 def all_reduce_gpu(t):
     return _op_lib.all_reduce_gpu(t, input_tensor_name=t.name[:-2])
 
+
 def ako_all_reduce(t, partition_id, num_partitions):
     return _op_lib.ako_negotiator(t, partition_id, num_partitions)
+
 
 def global_variance(t):
     return _op_lib.global_variance(t)
@@ -97,63 +81,129 @@ def set_num_gradients(n):
 def start_gpu_group(*args, **kwargs):
     return _op_lib.start_gpu_group(*args, **kwargs)
 
-# Based on Andrei-Octavian Brabete, Partitioning done within C++ operator
-def partial_exchange_group_all_reduce(ts, batch_size, num_train, fraction=0.3, accumulate=False, average="none"):
-    import math
-    total_size = sum([t.shape.num_elements() * t.dtype.size for t in ts])
-    print("Total Size of All Gradients: " + str(total_size))
+
+def _bin_pack(sizes, budget, adjust_budget=False):
+    lst = list(reversed(sorted([(size, name)
+                                for name, size in sizes.items()])))
+    if adjust_budget:
+        budget = max(budget, lst[0][0])
+    else:
+        raise RuntimeError("Budget is too small.")
+    budgets = []
+    indexes = dict()
+    for size, name in lst:
+        ok = False
+        for i, b in enumerate(budgets):
+            if b >= size:
+                budgets[i] -= size
+                indexes[name] = i
+                ok = True
+                break
+        if not ok:
+            budgets.append(budget - size)
+            indexes[name] = len(budgets) - 1
+    return indexes
+
+
+def _bin_pack(sizes, budget, adjust_budget=False):
+    lst = list(reversed(sorted([(size, name)
+                                for name, size in sizes.items()])))
+    if adjust_budget:
+        budget = max(budget, lst[0][0])
+    else:
+        raise RuntimeError("Budget is too small.")
+    budgets = []
+    indexes = dict()
+    for size, name in lst:
+        ok = False
+        for i, b in enumerate(budgets):
+            if b >= size:
+                budgets[i] -= size
+                indexes[name] = i
+                ok = True
+                break
+        if not ok:
+            budgets.append(budget - size)
+            indexes[name] = len(budgets) - 1
+    return indexes
+
+
+def _tensor_size(t):
+    return t.shape.num_elements() * t.dtype.size
+
+
+def _parse_schedule(schdule):
+    # schedule is of the form
+    # f1;e1;f2;e2;f3;e3
+    tokens = schedule.split(";")
+    epochs = [0] + [int(t) for t in tokens[1::2]]
+    pairs = []
+    for i in range(len(epochs) - 1):
+        pairs.append((epochs[i], epochs[i + 1]))
+    fractions = [float(t) for t in tokens[::2]]
+    return zip(pairs, fractions)
+
+
+def print_info(fraction, total_size, budget):
     print("The fraction is: " + str(fraction))
-    budget = int(math.floor(fraction * total_size))
+    print("Total Size of All Gradients: " + str(total_size))
     print("The bucket budget is: " + str(budget))
 
-    trained_steps_op = tf.Variable(tf.zeros([], tf.int32))
-    modify_trained_steps_op = tf.assign(
-            trained_steps_op, global_step_modifier(trained_steps_op))
 
-    with tf.control_dependencies([modify_trained_steps_op]):
-        return [partial_exchange_all_reduce(t, budget, len(ts), accumulate, average, fraction, num_train / (batch_size * get_num_peers())) for t in ts]
-
-# Based on Guo Li, Partitioning in python
-def partial_exchange_group_all_reduce_front_end_partitioning(ts, fraction=0.3, accumulate=False, average="none"):
+def adaptive_partial_exchange_with_cpu_allreduce(ts,
+                                                 batch_size,
+                                                 num_train,
+                                                 schedule,
+                                                 accumulate=False,
+                                                 average="none"):
+    print("Using piecewise partitioning schedule: " + schedule)
+    parsed_schedule = _parse_schedule(schedule)
     import math
-    total_size = sum([t.shape.num_elements() * t.dtype.size for t in ts])
-    print("Total Size of All Gradients: " + str(total_size))
-    print("The fraction is: " + str(fraction))
-    binpacker = BinPackPartitioner()
-    budget = int(math.floor(fraction * total_size))
-    indexes = binpacker.bin_pack(dict([(t.name, t.shape.num_elements() * t.dtype.size) for t in ts]), budget)
-    print("The bucket budget is: " + str(budget))
+    import tensorflow as tf
+    total_size = sum([_tensor_size(t) for t in ts])
 
+    def build_partial_exchange_ops(fraction):
+        budget = int(math.floor(fraction * total_size))
+        indexes = _bin_pack(dict((t.name, _tensor_size(t)) for t in ts),
+                            budget)
+        print_info(fraction, total_size, budget)
 
-    trained_steps_op = tf.Variable(tf.zeros([], tf.int32))
-    modify_trained_steps_op = tf.assign(
-            trained_steps_op, global_step_modifier(trained_steps_op))
+        gs = tf.Variable(tf.zeros([], dtype=tf.int64))
+        advance_gs = tf.assign(gs, gs + 1)
 
-    with tf.control_dependencies([modify_trained_steps_op]):
-        return [
-        # pass indexes[t.name] instead of budget
-            partial_exchange_all_reduce_front_end_partitioning(t, index=indexes[t.name], partitions=len(set(indexes.values()))) for t in ts
-        ]
+        num_partitions = len(set(indexes.values()))
 
+        # Construct groups
+        groups = [[] for _ in range(num_partitions)]
+        for t in ts:
+            groups[indexes[t.name]].append(t)
 
-def ako_group_all_reduce(gradient_tensors, num_partitions=1):
-    partitioner      = AkoPartitioner(num_partitions)
-    grads_and_vars_to_negotiate = [(grad, grad.name[:-2]) for grad in gradient_tensors]
-    partitionIndices = partitioner.partition_positions(grads_and_vars_to_negotiate)
-    partitions       = partitioner.reconstruct_partition(grads_and_vars_to_negotiate, partitionIndices)
-    
-    # Print info
-    partitioner.print_gradient_info(grads_and_vars_to_negotiate)
-    partitioner.print_partition_info(num_partitions, partitions)
-    
-    negotiated_grads = []
-    for partition_id in range(len(partitions)):
-        for grad, var in partitions[partition_id]:
-            with tf.variable_scope('AkoMaybeNegotiatedGrad'):
-                negotiated_grad = ako_all_reduce(grad,tf.constant([partition_id], dtype=tf.int32),
-                                                 tf.constant([num_partitions], dtype=tf.int32))
-            negotiated_grads.append(negotiated_grad)
-    return negotiated_grads
+        # Start all groups
+        reordered_cond_ops = [None] * len(ts)
+        for i, partition in enumerate(groups):
+            negotiated_partition = tf.cond(
+                tf.equal(tf.mod(gs - 1, num_partitions), i), lambda:
+                cpu_group_all_reduce(partition), lambda: partition)
+            for negotiated_grad, grad in zip(negotiated_partition, partition):
+                reordered_cond_ops[name_order[grad.name]] = negotiated_grad
+        return reordered_cond_ops
+
+    def tf_is_between_closed_open(x, left, right):
+        l = tf.math.logical_or(tf.greater(x, left), tf.equal(x, left))
+        r = tf.less(x, right)
+        return tf.math.logical_and(l, r)
+
+    with tf.control_dependencies([advance_gs]):
+        initial_partitioning_ops = build_partial_exchange_ops(fraction)
+
+        epoch = tf.div(gs, num_train / (batch_size * get_num_peers()))
+
+        cond_ops = []
+        for (pivot_epoch_l, pivot_epoch_r), pivot_fraction in parsed_schedule:
+            cond = tf.cond(tf_is_between_closed_open(epoch, pivot_epoch_l, pivot_epoch_r), lambda: build_partial_exchange_ops(pivot_fraction), lambda: tf.no_op(name="No Op Dynamic Partitioning"))
+            cond_ops.append(cond)
+        return cond_ops
+
 
 def cpu_group_all_reduce(ts):
     return [all_reduce(t) for t in ts]
