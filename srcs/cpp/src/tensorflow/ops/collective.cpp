8560d5a50336d8adac79e25cce6b86483cea55a6
lg <lg4869@outlook.com>
2019-05-14 18:28:47 +0800

    #include <tensorflow/core/framework/common_shape_fns.h>

--- srcs/cpp/src/tensorflow/ops/collective.cpp
+++ srcs/cpp/src/tensorflow/ops/collective.cpp
@@ -1,3 +1,4 @@
+#include <tensorflow/core/framework/common_shape_fns.h>
 #include <tensorflow/core/framework/op.h>
 #include <tensorflow/core/framework/op_kernel.h>
 #include <tensorflow/core/framework/shape_inference.h>
