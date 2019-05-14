7daa9aff2e147c0d6c7f5e4e2c4256350247c0c8
Andrei-Octavian Brabete <brabete.andrei96@gmail.com>
2019-05-14 12:56:08 +0100

    Add bin packer function

--- srcs/python/kungfu/ops.py
+++ srcs/python/kungfu/ops.py
@@ -134,6 +134,25 @@ def group_all_reduce(ts):
     print('USING CPU GROUP ALL REDUCE')
     return cpu_group_all_reduce(ts)
 
+def bin_pack(sizes, budget):
+    lst = list(reversed(sorted([(size, name) for name, size in sizes.items()])))
+    budget = max(budget, lst[0][0])
+    budgets = []
+    indexes = dict()
+    for size, name in lst:
+        ok = False
+        for i, b in enumerate(budgets):
+            if b >= size:
+                budgets[i] -= size
+                indexes[name] = i
+                ok = True
+                break
+        if not ok:
+            budgets.append(budget - size)
+            indexes[name] = len(budgets) - 1
+    return indexes
+
+
 def gpu_partial_exchange_group_all_reduce_front_end_partitioning(ts, fraction=0.3, accumulate=False, average="none"):
     import math
     total_size = sum([t.shape.num_elements() * t.dtype.size for t in ts])
