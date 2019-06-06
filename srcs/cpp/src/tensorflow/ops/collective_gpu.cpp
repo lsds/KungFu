a1404c93abaab67cd1b91cf1a9a9e3a97642b469
Luo Mai <luo.mai.cs@gmail.com>
2019-06-06 14:50:02 +0100

    fix

--- srcs/cpp/src/tensorflow/ops/collective_gpu.cpp
+++ srcs/cpp/src/tensorflow/ops/collective_gpu.cpp
@@ -59,14 +59,14 @@ class AllReduceGpu : public AsyncOpKernel
         const Tensor &input = context->input(0);
         Tensor *output      = nullptr;
         OP_REQUIRES_OK_ASYNC(
-             context, context->allocate_output(0, input.shape(), &output), done);
+            context, context->allocate_output(0, input.shape(), &output), done);
 
         kungfu::tensorflow::_world_gpu->AllReduce(
             [stream = context->op_device_context()->stream()]() {
                 // stream->BlockHostUntilDone();
                 cudaStream_t *cuda_stream = reinterpret_cast<cudaStream_t *>(
                     stream->implementation()->GpuStreamMemberHack());
-                cuda_stream->sync();
+                // cuda_stream->sync();
             },
             input.tensor_data().data(), (void *)(output->tensor_data().data()),
             input.NumElements(), to_kungfu_type(input.dtype()), KungFu_SUM,
