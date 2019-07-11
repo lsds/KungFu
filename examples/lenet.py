618785114837fb01beca5cf6d917c36e761bf494
Luo Mai <luo.mai.cs@gmail.com>
2019-07-11 13:28:52 +0100

    file naming

--- examples/lenet.py
+++ examples/lenet.py
@@ -108,12 +108,13 @@ def build_train_ops(kungfu_strategy, ako_partitions, device_batch_size):
     optimizer = tf.train.MomentumOptimizer(learning_rate=0.001, momentum=0.9)
 
     if kungfu_strategy == 'p2p':
-        from kungfu.optimizers import PeerModelAveraging
-        optimizer = PeerModelAveraging(optimizer)
+        from kungfu.optimizers import ModelAveragingOptimizer
+        print("Using ModelAveragingOptimizer")
+        optimizer = ModelAveragingOptimizer(optimizer)
     else:
-        from kungfu.optimizers import ParallelOptimizer
-        print("Using parallel optimizer")
-        optimizer = ParallelOptimizer(optimizer,
+        from kungfu.optimizers import SyncSGDOptimizer
+        print("Using SyncSGDOptimizer")
+        optimizer = SyncSGDOptimizer(optimizer,
                                       device_batch_size=device_batch_size)
 
     train_step = optimizer.minimize(loss, name='train_step')
@@ -133,8 +134,8 @@ def train_mnist(x, y, mnist, train_step, acc, n_epochs, n_batches, batch_size,
 
         initializer = None
         if kungfu_strategy == 'p2p':
-            from kungfu.optimizers import PeerModelAveraging
-            initializer = PeerModelAveraging.get_initializer()
+            from kungfu.optimizers import ModelAveragingOptimizer
+            initializer = ModelAveragingOptimizer.get_initializer()
         else:
             initializer = kf.distributed_variables_initializer()
 
--- srcs/python/kungfu/optimizers/model_avg.py
+++ srcs/python/kungfu/optimizers/model_avg.py
@@ -5,7 +5,7 @@ from kungfu.ops import broadcast, model_averaging, request_model, save_model
 from .core import KungFuOptimizer
 
 
-class PeerModelAveraging(KungFuOptimizer):
+class ModelAveragingOptimizer(KungFuOptimizer):
     """An optimizer that negotiates using the AllReduce operator."""
 
     def __init__(self,
@@ -17,8 +17,8 @@ class PeerModelAveraging(KungFuOptimizer):
                  use_locking=False,
                  device_dense='',
                  device_sparse=''):
-        super(PeerModelAveraging, self).__init__(optimizer, name, use_locking,
-                                                 device_dense, device_sparse)
+        super(ModelAveragingOptimizer, self).__init__(optimizer, name, use_locking,
+                                                      device_dense, device_sparse)
         self.request_mode = request_mode
         self.model_averaging_device = model_averaging_device
         self.peer_selection_strategy = peer_selection_strategy
