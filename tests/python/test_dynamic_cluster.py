7d928616d15536fc9322534be6559f8fdbb8b7aa
lg <lg4869@outlook.com>
2019-08-13 14:18:13 +0800

    update example

--- tests/python/test_dynamic_cluster.py
+++ tests/python/test_dynamic_cluster.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
+import argparse
+
 import os
 import tensorflow as tf
 
 from kungfu.ops import update_cluster, barrier, all_reduce, propose_update
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--max-peer', type=int, default=8, help='')
+    parser.add_argument('--max-step', type=int, default=24, help='')
+    return parser.parse_args()
+
+
+args = parse_args()
 
 init_sess = os.getenv('KUNGFU_INIT_SESS')
 init_gs = int(init_sess)
@@ -16,21 +28,17 @@ best_size = tf.Variable(tf.ones([], dtype=tf.int32))
 propose = propose_update(gs + 1, best_size)
 update = update_cluster(gs)
 
-host_cap = 4
-
 version = tf.Variable(tf.constant(init_sess, dtype=tf.string))
 
 
 def compute_new_size(prev_size):
-    return tf.mod(prev_size, host_cap) + 1
+    return tf.mod(prev_size, args.max_peer) + 1
     # return tf.cast(gs + 1, tf.int32)
 
 
 # kf_init = Opt.get_init()
 
 compute_new_size = tf.assign(best_size, compute_new_size(best_size))
-
-max_step = 100
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
@@ -39,7 +47,7 @@ with tf.Session() as sess:
     # sess.run(kf_init) ->
     while True:
         step = sess.run(gs)
-        if step > max_step:
+        if step > args.max_step:
             break
 
         if need_update:
@@ -60,7 +68,7 @@ with tf.Session() as sess:
 
         sess.run(compute_new_size)
 
-        if step < max_step:
+        if step < args.max_step:
             need_update = sess.run(propose)
 
         sess.run(inc_gs)
