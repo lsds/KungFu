b7ccce7b2f2f3acbb93951724497d52d3ff32a6f
lg <lg4869@outlook.com>
2019-09-13 21:44:16 +0800

    add cluster_schedule

--- benchmarks/adaptive_trainer.py
+++ benchmarks/adaptive_trainer.py
@@ -23,18 +23,30 @@ advance_version = tf.assign_add(version, 1)
 global_step = tf.Variable(get_start_step(version), trainable=False)
 advance_global_step = tf.assign_add(global_step, 1)
 
-new_cluster_size = tf.Variable(3 * tf.ones([], dtype=tf.int32))
+cluster_schedule = [
+    # (gs, new_size) :: scale cluster to new_size after step gs
+    (3, 3),
+    (5, 4),
+]
+
+propose_steps = [gs for gs, _ in cluster_schedule]
+update_steps = [gs + 1 for gs, _ in cluster_schedule]
+
+
+def compute_new_size(schedule, global_step, init):
+    schedule = reversed(sorted(schedule))
+    return tf.case([(tf.greater_equal(
+        global_step, step), lambda size=size: tf.constant(size))
+                    for step, size in schedule], lambda: tf.constant(init))
+
+
+new_cluster_size = compute_new_size(cluster_schedule, global_step, 0)
 propose_op = propose_update(global_step + 1, version, new_cluster_size)
 update_op = update_cluster(version)
 
 init = tf.global_variables_initializer()
 
 max_step = 10
-
-# change size 2 -> 3 at gs = 3
-# gs : 1 2 3 4 5 6 7 8 9 10
-#          *
-# np : 2 2 2 3 3 3 3 3 3 3
 
 with tf.Session() as sess:
     sess.run(init)
@@ -45,19 +57,23 @@ with tf.Session() as sess:
         gs = sess.run(global_step)
         print('step: %d' % (gs))
 
-        if gs == 4:
+        # BEGIN hook
+        if gs in update_steps:
             sess.run(update_op)
+        # END hook
 
         v = sess.run(y)
         # sess.run(fake_train_op)
         print(v)
 
-        if gs == 3:
+        # BEGIN hook
+        if gs in propose_steps:
             sess.run(advance_version)
             # all peers write to config server with idential content (the new cluster size)
             _, keep = sess.run(propose_op)
             if not keep:
                 break
+        # END hook
 
         gs = sess.run(advance_global_step)
         if gs >= max_step:
