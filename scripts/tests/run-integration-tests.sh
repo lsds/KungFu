c23430710cbf7ca0e96d7f0555658bfb8c0ac3c0
LG <lgarithm@users.noreply.github.com>
2019-09-27 00:32:54 +0800

    import memory pool (#118)

--- scripts/tests/run-integration-tests.sh
+++ scripts/tests/run-integration-tests.sh
@@ -11,6 +11,7 @@ reinstall() {
     CMAKE_SOURCE_DIR=$(pwd)
     export CGO_CFLAGS="-I${CMAKE_SOURCE_DIR}/srcs/cpp/include"
     export CGO_LDFLAGS="-L${CMAKE_SOURCE_DIR}/lib -lkungfu-base -lstdc++"
+    export CGO_CXXFLAGS="-std=c++11"
 
     ./scripts/go-install.sh
 }
