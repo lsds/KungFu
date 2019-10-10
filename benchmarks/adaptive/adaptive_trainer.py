e93480cd27ef097dfe4de8f9c22cb5bd71b816ef
lg <lg4869@outlook.com>
2019-10-10 17:10:05 +0800

    fix adaptation schedule

--- benchmarks/adaptive/adaptive_trainer.py
+++ benchmarks/adaptive/adaptive_trainer.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from kungfu.ops import all_reduce, current_cluster_size
 from kungfu.ops.adapt import get_init_checkpoint, resize_cluster
 
-p = argparse.ArgumentParser(description='TF ML Benchmarks.')
+p = argparse.ArgumentParser(description='Adaptation Benchmark.')
 p.add_argument('--schedule',
                type=str,
                default='3:2,3:4,3:16,3:1',
@@ -24,20 +24,21 @@ def parse_schedule(config):
     for p in config.split(','):
         kv = p.split(':')
         n, val = (int(kv[0]), int(kv[1]))
+        schedule.append((t, t + n, val))
         t += n
-        schedule.append((t, val))
     return schedule, t
 
 
 cluster_size_schedule, max_step = parse_schedule(args.schedule)
-print(cluster_size_schedule)
-print(max_step)
+# print(cluster_size_schedule)
+# print(max_step)
 
 
-def get_new_size(i, sch, old):
-    for j, n in reversed(sch):
-        if i >= j:
+def get_cluster_size(i, sch, old):
+    for s, e, n in sch:
+        if s <= i and i < e:
             return n
+    print('[W] not scheduled for %d' % (i))
     return old
 
 
@@ -60,20 +61,20 @@ with tf.Session() as sess:
     sess.run(init)
 
     init_gs = restore(get_init_checkpoint())
-    print('restored to %d' % (init_gs))
     np = current_cluster_size()
-    init_np = get_new_size(init_gs, cluster_size_schedule, np)
+    init_np = get_cluster_size(init_gs, cluster_size_schedule, np)
+    print('restored to %d, np=%d, init_np=%d' % (init_gs, np, init_np))
     if np != init_np:
         print(
-            '[W] init cluster size (%d) is not consistent with schedule (%d)' %
-            (np, init_np))
+            '[W] init cluster size (np=%d) is not consistent with schedule (np=%d)'
+            % (np, init_np))
 
     for gs in range(init_gs, max_step):
         v = sess.run(y)
         print('step %d, result: %d' % (gs, v))
 
         next_gs = gs + 1
-        np = get_new_size(next_gs, cluster_size_schedule, np)
+        np = get_cluster_size(next_gs, cluster_size_schedule, np)
         keep = sess.run(resize_op,
                         feed_dict={
                             ckpt: str(next_gs),
