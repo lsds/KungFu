255145b87b9985842ce64c4cdef5b63e6fd69074
Luo Mai <luo.mai.cs@gmail.com>
2019-10-15 16:15:23 +0100

    Remove fp16 compression.

--- benchmarks/performance/benchmark_horovod.py
+++ benchmarks/performance/benchmark_horovod.py
@@ -21,10 +21,6 @@ import horovod.tensorflow as hvd
 parser = argparse.ArgumentParser(
     description='TensorFlow Synthetic Benchmark',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--fp16-allreduce',
-                    action='store_true',
-                    default=False,
-                    help='use fp16 compression during allreduce')
 parser.add_argument('--model',
                     type=str,
                     default='ResNet50',
@@ -77,9 +73,6 @@ if args.eager:
 model = getattr(applications, args.model)(weights=None)
 
 opt = tf.train.GradientDescentOptimizer(0.01)
-
-# Horovod: (optional) compression algorithm.
-compression = hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.none
 
 # Horovod: wrap optimizer with DistributedOptimizer.
 opt = hvd.DistributedOptimizer(opt, device_dense='/cpu:0')
