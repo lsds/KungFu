b49139f7f4384388ee36699a8f6d5c5491a1bd3f
lg <lg4869@outlook.com>
2019-10-19 14:20:34 +0800

    bypass np=1

--- srcs/python/kungfu/ops/collective.py
+++ srcs/python/kungfu/ops/collective.py
@@ -18,11 +18,16 @@ def _group_all_reduce(ts):
     return [all_reduce(t) for t in ts]
 
 
-def group_all_reduce(ts):
+def _maybe_group_all_reduce(ts, group_all_reduce_fn):
+    # a helper function to bypass all_reduce for np = 1
     _rank, np = peer_info()
     import tensorflow as tf
-    return tf.cond(np > 1, lambda: _group_all_reduce(ts),
+    return tf.cond(np > 1, lambda: group_all_reduce_fn(ts),
                    lambda: [tf.identity(t) for t in ts])
+
+
+def group_all_reduce(ts):
+    return _maybe_group_all_reduce(ts, _group_all_reduce)
 
 
 def _nccl_all_reduce(t):
@@ -33,7 +38,7 @@ def _start_nccl_scheduler(*args, **kwargs):
     return _op_lib.start_gpu_group(*args, **kwargs)
 
 
-def group_nccl_all_reduce(ts):
+def _group_nccl_all_reduce(ts):
     names = [t.name for t in ts]
     names = list(sorted(names))  # FIXME: use topsort
     import tensorflow as tf
@@ -41,3 +46,7 @@ def group_nccl_all_reduce(ts):
             _start_nccl_scheduler(names),
     ]):
         return [_nccl_all_reduce(t) for t in ts]
+
+
+def group_nccl_all_reduce(ts):
+    return _maybe_group_all_reduce(ts, _group_nccl_all_reduce)
