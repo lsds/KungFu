080bcc8dbf3352c18ae4cb392f1c4be0a79ee6a0
lg <lg4869@outlook.com>
2019-10-23 13:40:22 +0800

    add builtin benchmark

--- srcs/python/kungfu/helpers/utils.py
+++ srcs/python/kungfu/helpers/utils.py
@@ -1,7 +1,9 @@
+Ki = 1024
+Mi = Ki * Ki
+Gi = Mi * Ki
+
+
 def show_size(s):
-    Ki = 1024
-    Mi = Ki * Ki
-    Gi = Mi * Ki
     if s > Gi:
         return '%.2fGi' % (float(s) / Gi)
     elif s > Mi:
@@ -10,3 +12,15 @@ def show_size(s):
         return '%.2fKi' % (float(s) / Ki)
     else:
         return '%d' % s
+
+
+def show_rate(size, duration):
+    r = size / duration
+    if r < Ki:
+        return '%.2fB/s' % r
+    elif r < Mi:
+        return '%.2fKiB/s' % (r / Ki)
+    elif r < Gi:
+        return '%.2fMiB/s' % (r / Mi)
+    else:
+        return '%.2fGiB/s' % (r / Gi)
