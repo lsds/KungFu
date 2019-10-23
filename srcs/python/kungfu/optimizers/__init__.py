c891c4ad0693cbd9124b837e9039d665834f3df7
Luo Mai <luo.mai.cs@gmail.com>
2019-10-23 18:10:38 +0100

    Adaptive SGD and synchronous model averaging SGD. (#163)
    * Adaptive SGD.
    
    * Check in ada-sgd
    
    * Use model averaging to implement sync SGD.
    
    * Adaptive SGD converges on ResNet-50 benchmark.
    
    * update name
    
    * Simplify ada SGD
    
    * Fix lint
    
    * Add a SMA optimizer.
    
    * add SMA into the benchmark
    
    * fix lint
    
    * add comments
    
    * Add comments for SMA.
    
    * fix format

--- srcs/python/kungfu/optimizers/__init__.py
+++ srcs/python/kungfu/optimizers/__init__.py
@@ -1,3 +1,5 @@
+from .ada_sgd import AdaptiveSGDOptimizer
 from .async_sgd import PeerModelAveragingOptimizer
+from .sma_sgd import SyncModelAveragingSGDOptimizer
 from .sync_sgd import (SyncSGDOptimizer, SyncSGDWithGradNoiseScaleOptimizer,
                        SyncSGDWithGradVarianceOptimizer)
