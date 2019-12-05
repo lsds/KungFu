b4a922257ad793172de1af2392cf3e726a3720f2
lg <lg4869@outlook.com>
2019-12-05 11:04:38 +0000

    fix

--- srcs/cpp/src/tensorflow/ops/cpu/consensus.cpp
+++ srcs/cpp/src/tensorflow/ops/cpu/consensus.cpp
@@ -8,27 +8,29 @@ REGISTER_KUNGFU_OP(Consensus)
     .Attr("T: {int32, int64, float16, float32, float64}")
     .Input("input: T");
 
-class Consensus : public AsyncOpKernel
+class Consensus : public OpKernel
 {
     std::string tensor_name_;
 
   public:
-    explicit Consensus(OpKernelConstruction *context) : AsyncOpKernel(context)
+    explicit Consensus(OpKernelConstruction *context) : OpKernel(context)
     {
         OP_REQUIRES_OK(context, context->GetAttr("tensor_name", &tensor_name_));
         OP_REQUIRES(context, !tensor_name_.empty(),
                     errors::InvalidArgument("tensor_name is empty"));
     }
 
-    void ComputeAsync(OpKernelContext *context, DoneCallback done) override
+    void Compute(OpKernelContext *context) override
     {
         const Tensor &input = context->input(0);
         bool ok;
         _kungfu_world->Consensus(
             input.tensor_data().data(), input.NumElements(),
-            to_kungfu_type(input.dtype()), &ok, tensor_name_.c_str(), done);
-        if (!ok) {
-            LOG(ERROR) << "Consensus check for " << tensor_name_ << " failed";
+            to_kungfu_type(input.dtype()), &ok, tensor_name_.c_str());
+        if (ok) {
+            LOG(ERROR) << "Consensus check OK for " << tensor_name_;
+        } else {
+            LOG(ERROR) << "Consensus check FAILED for " << tensor_name_;
         }
     }
 };
