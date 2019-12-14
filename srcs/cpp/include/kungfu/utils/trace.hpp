ab824b7fc1ff41f89f6ce8ec45d3aeab99c6ef23
lg <lg4869@outlook.com>
2019-12-14 12:30:45 +0000

    update trace

--- srcs/cpp/include/kungfu/utils/trace.hpp
+++ srcs/cpp/include/kungfu/utils/trace.hpp
@@ -1,29 +1,7 @@
 #pragma once
 #ifdef KUNGFU_ENABLE_TRACE
 
-#include <stdtracer.hpp>
-
-using tracer_t = simple_tracer_t;
-
-extern simple_tracer_ctx_t default_simple_ctx;
-// extern log_tracer_ctx_t default_log_ctx;
-
-#define DEFINE_TRACE_CONTEXTS simple_tracer_ctx_t default_simple_ctx("global");
-
-#define TRACE_SCOPE(name)                                                      \
-    tracer_t _((name), default_simple_ctx /*, default_log_ctx */)
-
-#define TRACE_STMT(e)                                                          \
-    {                                                                          \
-        tracer_t _(#e, default_simple_ctx /*, default_log_ctx */);             \
-        e;                                                                     \
-    }
-
-#define TRACE_EXPR(e)                                                          \
-    [&]() {                                                                    \
-        tracer_t _(#e, default_simple_ctx /*, default_log_ctx */);             \
-        return (e);                                                            \
-    }()
+#include <stdtracer_thread>
 
 #else
 
