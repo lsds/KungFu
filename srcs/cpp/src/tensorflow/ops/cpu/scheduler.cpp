a33d7a043fc1a1715ff0e1568d6192de2548b595
lg <lg4869@outlook.com>
2019-12-17 11:34:34 +0000

    cleanup

--- srcs/cpp/src/tensorflow/ops/cpu/scheduler.cpp
+++ srcs/cpp/src/tensorflow/ops/cpu/scheduler.cpp
@@ -4,17 +4,16 @@ namespace tensorflow
 {
 REGISTER_KUNGFU_OP(StartNcclScheduler).Input("input: string");
 
-std::string show_list(const std::vector<int32_t> &a)
-{
-    std::string line;
-    for (auto x : a) { line += ", " + std::to_string(x); }
-    return line;
-}
-
 class StartNcclScheduler : public OpKernel
 {
     int counter_;
     std::vector<int32_t> permu;
+
+    void ResetOrder(int n)
+    {
+        permu.resize(n);
+        std::iota(permu.begin(), permu.end(), 0);
+    }
 
   public:
     explicit StartNcclScheduler(OpKernelConstruction *context)
@@ -30,22 +29,17 @@ class StartNcclScheduler : public OpKernel
         for (int i = 0; i < t_names.size(); ++i) {
             names.push_back(t_names(i));
         }
-        if (counter_ > 0) {
+        if (names.size() != permu.size()) { ResetOrder(names.size()); }
+        if (kungfu::_nccl_order_group.get() != nullptr) {
             if (counter_ == 1) {
                 const std::vector<int32_t> arrive_order =
                     kungfu::_nccl_order_group->Wait();
-                // const auto line = show_list(arrive_order);
-                // fprintf(stderr, "arrive_order|%d| %s\n", counter_,
-                // line.c_str());
                 if (arrive_order.size() == permu.size()) {
                     _kungfu_world->Broadcast(
                         arrive_order.data(), permu.data(), permu.size(),
                         to_kungfu_type(DT_INT32), name().c_str());
                 }
             }
-        } else {
-            permu.resize(names.size());
-            std::iota(permu.begin(), permu.end(), 0);
         }
         kungfu::_nccl_order_group.reset(new kungfu::order_group(names, permu));
         ++counter_;
