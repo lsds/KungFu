9e6ede77e4acec9fb8e9c4b2b2da29b058850920
lg <lg4869@outlook.com>
2019-12-23 17:34:54 +0000

    _show_tensorflow_info

--- srcs/python/kungfu/info/__main__.py
+++ srcs/python/kungfu/info/__main__.py
@@ -9,9 +9,19 @@ import sys
 from kungfu.ext import show_cuda_version, show_nccl_version
 
 
+def _show_tensorflow_info():
+    try:
+        import tensorflow as tf
+        print('Tensorflow Version: %s' % (tf.__version__))
+    except:
+        print('Tensorflow is NOT installed')
+    pass
+
+
 def main(_):
     show_cuda_version()
     show_nccl_version()
+    _show_tensorflow_info()
 
 
 if __name__ == "__main__":
