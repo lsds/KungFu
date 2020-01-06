9a381815abe152a1404ddf7cf91daf943fbf8da8
lg <lg4869@outlook.com>
2020-01-06 20:52:22 +0000

    cleanup log

--- tests/python/integration/test_elastic_estimator.py
+++ tests/python/integration/test_elastic_estimator.py
@@ -1,9 +1,10 @@
 import argparse
 import os
 
+import numpy as np
 import tensorflow as tf
 from kungfu.tensorflow.initializer import BroadcastGlobalVariablesOp
-from kungfu.tensorflow.ops import (all_reduce, broadcast, counter, consensus,
+from kungfu.tensorflow.ops import (all_reduce, broadcast, consensus, counter,
                                    get_init_checkpoint, resize_cluster,
                                    step_based_schedule)
 from kungfu.tensorflow.optimizers import SynchronousSGDOptimizer
@@ -40,37 +41,30 @@ def model_fn(features, labels, mode):
     eval_metric_ops = {
         'accuracy': tf.metrics.accuracy(labels=labels, predictions=predictions)
     }
-
     optimizer = tf.train.GradientDescentOptimizer(0.1)
     optimizer = SynchronousSGDOptimizer(optimizer)
     train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
-
     return tf.estimator.EstimatorSpec(mode=mode,
                                       loss=loss,
                                       train_op=train_op,
                                       eval_metric_ops=eval_metric_ops)
 
 
-def input_fn(ds, batch_size, epochs=1):
+def input_fn(ds, batch_size, epochs=1, shuffle=True):
     features = {'x': ds.images}
     return tf.estimator.inputs.numpy_input_fn(x=features,
                                               y=ds.labels,
                                               batch_size=batch_size,
                                               num_epochs=epochs,
-                                              shuffle=False)
+                                              shuffle=shuffle)
 
 
 class KungFuElasticTrainHook(tf.train.SessionRunHook):
-    def __init__(self, schedule, max_step):
+    def __init__(self, schedule, max_step, model_dir):
         self._schedule = schedule
         self._max_step = max_step
-        self._counter = 0
-        self._verbose = False
+        self._model_dir = model_dir
         self._need_sync = True
-
-    def log(self, msg):
-        if self._verbose:
-            print(msg)
 
     def _build_resize_op(self, config, init_step):
         step = counter(init_step)
@@ -80,20 +74,9 @@ class KungFuElasticTrainHook(tf.train.SessionRunHook):
         return resize_op
 
     def begin(self):
-        self.log('begin')
         self._kungfu_step = tf.Variable(0, trainable=False, dtype=tf.int64)
         self._advance = tf.assign_add(self._kungfu_step, 1)
-
-        vs = tf.global_variables()
-        print('%d global variables' % (len(vs)))
-        for v in vs:
-            print('%s :: %s %s' % (v.name, str(v.dtype), v.shape))
-
-        model_vs = [v for v in vs if 'dense' in v.name]
-
         self._sync_op = BroadcastGlobalVariablesOp()
-        self._consensus_ops = [consensus(v) for v in model_vs]
-
         ckpt = os.getenv('KUNGFU_INIT_CKPT')
         self._init_kungfu_step = tf.assign(self._kungfu_step, int(ckpt))
         self._resize_op = self._build_resize_op(self._schedule, int(ckpt))
@@ -101,17 +84,10 @@ class KungFuElasticTrainHook(tf.train.SessionRunHook):
                                             int(ckpt))
 
     def after_create_session(self, sess, coord):
-        self.log('after_create_session')
         sess.run(self._init_kungfu_step)
         sess.run(self._reset_global_step)
 
-        global_step = sess.run(tf.train.get_global_step())
-        kungfu_step = sess.run(self._kungfu_step)
-        print('session created with global_step: %d, kungfu_step: %d' %
-              (global_step, kungfu_step))
-
     def before_run(self, run_context):
-        self.log('before_run: %d' % (self._counter))
         kungfu_step = run_context.session.run(self._kungfu_step)
         if kungfu_step >= self._max_step:
             print('request_stop before kungfu_step: %d' % (kungfu_step))
@@ -119,28 +95,20 @@ class KungFuElasticTrainHook(tf.train.SessionRunHook):
             # FIXME: force quit
 
         if self._need_sync:
-            print('running sync op')
-            ps = run_context.session.run(self._consensus_ops)
             run_context.session.run(self._sync_op)
-            qs = run_context.session.run(self._consensus_ops)
-            print('before sync: %s, after sync: %s' % (ps, qs))
             self._need_sync = False
 
     def after_run(self, run_context, run_values):
-        self.log('after_run: %d' % (self._counter))
-        self._counter += 1
-
         kungfu_step = run_context.session.run(self._kungfu_step)
-
         changed, keep = run_context.session.run(self._resize_op)
         if changed:
             print('changed on %d' % (kungfu_step))
             self._need_sync = True
             if not keep:
                 run_context.request_stop()
+                return
 
-        run_context.session.run(self._advance)
-        kungfu_step = run_context.session.run(self._kungfu_step)
+        kungfu_step = run_context.session.run(self._advance)
         if kungfu_step >= self._max_step:
             print('request_stop on kungfu_step: %d' % (kungfu_step))
             run_context.request_stop()
@@ -150,7 +118,17 @@ class KungFuElasticTrainHook(tf.train.SessionRunHook):
         kungfu_step = sess.run(self._kungfu_step)
         print('stopped at global_step: %d, kungfu_step: %d' %
               (global_step, kungfu_step))
-        self.log('end')
+
+        self.save(sess, 'final')
+
+    def save(self, sess, idx):
+        vs = tf.global_variables()
+        d = dict()
+        for t in vs:
+            v = sess.run(t)
+            d[t.name] = v
+        np.savez(os.path.join(self._model_dir, 'variables-%s.npz' % (idx)),
+                 **d)
 
 
 def get_model_dir(args):
@@ -163,18 +141,21 @@ def get_model_dir(args):
 def main():
     args = parse_args()
     print('using config: %s, max step=%d' % (args.schedule, args.max_step))
+    model_dir = get_model_dir(args)
 
     data = load_datasets(args.data_dir, normalize=True)
-    classifier = tf.estimator.Estimator(model_fn,
-                                        model_dir=get_model_dir(args))
+    classifier = tf.estimator.Estimator(model_fn, model_dir=model_dir)
 
-    classifier.train(
-        input_fn(data.train, 1000),
-        hooks=[KungFuElasticTrainHook(args.schedule, args.max_step)],
-        max_steps=args.max_step)
+    classifier.train(input_fn(data.train, 1000),
+                     hooks=[
+                         KungFuElasticTrainHook(args.schedule, args.max_step,
+                                                model_dir)
+                     ],
+                     max_steps=args.max_step)
 
-    print('train finished')
-    results = classifier.evaluate(input_fn(data.test, 1000), hooks=[], steps=1)
+    results = classifier.evaluate(input_fn(data.test, 1000, shuffle=False),
+                                  hooks=[],
+                                  steps=1)
     print('results: %s' % (results, ))
 
 
