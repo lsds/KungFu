452206e3c5ede618341e87df7008da8e4e5c8856
lg <lg4869@outlook.com>
2020-02-22 23:28:32 +0000

    PORT_RANGE=20001-20016

--- benchmarks/adaptation/bench-adaptation.sh
+++ benchmarks/adaptation/bench-adaptation.sh
@@ -9,6 +9,9 @@ timeout=2m
 cap=16
 H=127.0.0.1:$cap
 
+# travis-ci 10010: bind: address already in use
+PORT_RANGE=20001-20016
+
 kungfu_run() {
     local init_np=$1
     shift
@@ -17,6 +20,7 @@ kungfu_run() {
         -H ${H} \
         -np $init_np \
         -timeout ${timeout} \
+        -port-range ${PORT_RANGE} \
         -w \
         $@
 }
@@ -24,6 +28,4 @@ kungfu_run() {
 TF_CPP_MIN_LOG_LEVEL=2
 
 kungfu_run 2 python3 adaptive_trainer.py
-
-# travis-ci 10010: bind: address already in use
-kungfu_run 2 -port-range 20001-20016 python3 adaptive_trainer.py --schedule '1:16,1:1,1:16,1:1'
+kungfu_run 2 python3 adaptive_trainer.py --schedule '1:16,1:1,1:16,1:1'
