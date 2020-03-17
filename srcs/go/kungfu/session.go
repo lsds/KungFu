c54cea3ffcd8e4b9c64922c4e53961182a91bfc5
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2020-03-17 19:32:19 +0100

    increase timeout to 1min

--- srcs/go/kungfu/session.go
+++ srcs/go/kungfu/session.go
@@ -14,7 +14,7 @@ import (
 )
 
 const defaultRoot = 0
-const timeoutDuration = 30 * time.Second
+const timeoutDuration = 60 * time.Second
 
 // A strategy is a pair of dataflow graphs
 type strategy struct {
