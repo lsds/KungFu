ea75bd0b2bdeeb0b146f1a34a55d2042f03b2797
Konstantinos Fertakis <fertakis@gmail.com>
2020-03-18 20:31:12 +0000

    minor modif for enabling estimator

--- examples/tf1_training_monitoring.py
+++ examples/tf1_training_monitoring.py
@@ -238,18 +238,18 @@ def main(_):
     # model_dir  = "~/repos/KungFu/mnist/model"
 
     #TODO: check if estimator session necessary
-    save_checkpoints_steps = 100
-    save_summary_steps = 1 
-    config = tf.estimator.RunConfig(
-        model_dir=model_dir,
-        save_checkpoints_steps=save_checkpoints_steps,
-        save_summary_steps=save_summary_steps)
+    # save_checkpoints_steps = 100
+    # save_summary_steps = 1 
+    # config = tf.estimator.RunConfig(
+    #     model_dir=model_dir,
+    #     save_checkpoints_steps=save_checkpoints_steps,
+    #     save_summary_steps=save_summary_steps)
 
-    mnist_classifier = tf.estimator.Estimator(model_fn=model_function,
-                                              model_dir=model_dir,
-                                              config=config)
     # mnist_classifier = tf.estimator.Estimator(model_fn=model_function,
-    #                                           model_dir=model_dir)
+    #                                           model_dir=model_dir,
+    #                                           config=config)
+    mnist_classifier = tf.estimator.Estimator(model_fn=model_function,
+                                              model_dir=model_dir)
 
     num_train_steps = 10000
     
