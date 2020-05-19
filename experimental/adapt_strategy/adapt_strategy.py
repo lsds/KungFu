11c995a5aa21d250177c17b8c3bc4d3bac44f2ce
Konstantinos Fertakis <fertakis@gmail.com>
2020-05-19 17:50:05 +0100

    refactored for real training

--- experimental/adapt_strategy/adapt_strategy.py
+++ experimental/adapt_strategy/adapt_strategy.py
@@ -1,164 +1,231 @@
+#!/usr/bin/env python3
 """
-Usage:
-    kungfu-run -q -np 4 python3 -m kungfu.tensorflow.v1.benchmarks --method CPU
-    kungfu-run -q -np 4 python3 -m kungfu.tensorflow.v1.benchmarks --method NCCL
-    mpirun -np 4 python3 -m kungfu.tensorflow.v1.benchmarks --method HOROVOD
+Implemented based on:
+https://github.com/uber/horovod/blob/master/examples/tensorflow_synthetic_benchmark.py
 """
+
+from __future__ import absolute_import, division, print_function
 
 import argparse
-import sys
+import os
+import timeit
 
+import numpy as np
 import tensorflow as tf
-from kungfu._utils import measure, one_based_range, map_maybe
-from kungfu.ext import _get_cuda_index, change_strategy, log_stats, print_strategy_stats
-from kungfu.tensorflow.ops import (current_cluster_size, current_rank,
-                                   group_all_reduce, group_nccl_all_reduce,
-                                   monitored_all_reduce)
-from kungfu.tensorflow.v1.benchmarks import model_sizes
-from kungfu.tensorflow.v1.helpers.utils import show_rate, show_size
+from tensorflow.keras import applications
 from tensorflow.python.util import deprecation
+from kungfu.ext import change_strategy, log_stats, print_strategy_stats
+from kungfu import current_rank
 
 deprecation._PRINT_DEPRECATION_WARNINGS = False
-
 default_strategy_master = 0
 
-def _tensor_size(t):
-    return t.shape.num_elements() * t.dtype.size
+# Benchmark settings
+parser = argparse.ArgumentParser(
+    description='TensorFlow Synthetic Benchmark',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+parser.add_argument('--model',
+                    type=str,
+                    default='ResNet50',
+                    help='model to benchmark')
+parser.add_argument('--batch-size',
+                    type=int,
+                    default=32,
+                    help='input batch size')
+parser.add_argument(
+    '--num-warmup-batches',
+    type=int,
+    default=10,
+    help='number of warm-up batches that don\'t count towards benchmark')
+parser.add_argument('--num-batches-per-iter',
+                    type=int,
+                    default=10,
+                    help='number of batches per benchmark iteration')
+parser.add_argument('--num-iters',
+                    type=int,
+                    default=10,
+                    help='number of benchmark iterations')
+parser.add_argument('--eager',
+                    action='store_true',
+                    default=False,
+                    help='enables eager execution')
+parser.add_argument('--no-cuda',
+                    action='store_true',
+                    default=False,
+                    help='disables CUDA training')
+parser.add_argument('--kf-optimizer',
+                    type=str,
+                    default='sync-sgd',
+                    help='KungFu optimizers')
+parser.add_argument('--optimizer',
+                    type=str,
+                    default='sgd',
+                    help='Optimizer: sgd, adam')
+parser.add_argument('--fuse',
+                    action='store_true',
+                    default=False,
+                    help='Fuse KungFu operations')
+parser.add_argument('--adapt',
+                    action='store_true',
+                    default=False,
+                    help='Strategy Adaptation')
 
-def hvd_init():
-    import horovod.tensorflow as hvd
-    hvd.init()
+args = parser.parse_args()
+args.cuda = not args.no_cuda
 
-
-def hvd_group_all_reduce(ts):
-    import horovod.tensorflow as hvd
-    return [hvd.allreduce(t, average=False) for t in ts]
-
-
-def get_cluster_size(method):
-    if method == 'HOROVOD':
-        import horovod.tensorflow as hvd
-        return hvd.size()
-    else:
-        return current_cluster_size()
-
-
-_group_all_reduce_func = {
-    'CPU': group_all_reduce,
-    'NCCL': group_nccl_all_reduce,
-    'HOROVOD': hvd_group_all_reduce,
-}
-
-_model_sizes = {
-    'ResNet50': model_sizes.resnet50_imagenet,
-    'VGG16': model_sizes.vgg16_imagenet,
-    'BERT': model_sizes.bert,
-}
-
-
-def _config(method):
-    config = tf.ConfigProto()
+config = tf.ConfigProto()
+if args.cuda:
     config.gpu_options.allow_growth = True
-    if method == 'HOROVOD':
-        import horovod.tensorflow as hvd
-        config.gpu_options.visible_device_list = str(hvd.local_rank())
+    from kungfu.ext import _get_cuda_index
+    config.gpu_options.visible_device_list = str(_get_cuda_index())
+else:
+    config.gpu_options.allow_growth = False
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+    config.gpu_options.visible_device_list = ''
+
+if args.eager:
+    tf.enable_eager_execution(config)
+
+# Set up standard model.
+model = getattr(applications, args.model)(weights=None)
+
+opt = None
+learning_rate = 0.01
+if args.optimizer == 'sgd':
+    opt = tf.train.GradientDescentOptimizer(learning_rate)
+elif args.optimizer == 'adam':
+    opt = tf.train.AdamOptimizer(learning_rate)
+else:
+    raise Exception('Unknown optimizer option')
+
+barrier_op = None
+
+if args.kf_optimizer:
+    from kungfu.tensorflow.ops import barrier
+    barrier_op = barrier()
+    if args.kf_optimizer == 'sync-sgd':
+        from kungfu.tensorflow.optimizers import SynchronousSGDOptimizer
+        opt = SynchronousSGDOptimizer(opt)
+    elif args.kf_optimizer == 'sync-sgd-nccl':
+        from kungfu.tensorflow.optimizers import SynchronousSGDOptimizer
+        opt = SynchronousSGDOptimizer(opt, nccl=True, nccl_fusion=args.fuse)
+    elif args.kf_optimizer == 'async-sgd':
+        from kungfu.tensorflow.optimizers import PairAveragingOptimizer
+        opt = PairAveragingOptimizer(opt, fuse_requests=args.fuse)
+    elif args.kf_optimizer == 'sma':
+        from kungfu.tensorflow.optimizers import SynchronousAveragingOptimizer
+        opt = SynchronousAveragingOptimizer(opt)
     else:
-        config.gpu_options.visible_device_list = str(_get_cuda_index())
-    return config
+        raise Exception('Unknown kungfu option')
+
+data = tf.random_uniform([args.batch_size, 224, 224, 3])
+target = tf.random_uniform([args.batch_size, 1],
+                           minval=0,
+                           maxval=999,
+                           dtype=tf.int64)
 
 
-def _rank(method):
-    if method == 'HOROVOD':
-        import horovod.tensorflow as hvd
-        return hvd.rank()
-    else:
-        return current_rank()
-
-def parse_args():
-    p = argparse.ArgumentParser(description='Perf Benchmarks.')
-    p.add_argument('--model',
-                   type=str,
-                   default='ResNet50',
-                   help='ResNet50 | VGG16 | BERT')
-
-    p.add_argument('--method',
-                   type=str,
-                   default='CPU',
-                   help='CPU | NCCL | HOROVOD')
-    p.add_argument('--fuse', action='store_true', default=False, help='')
-
-    p.add_argument('--adapt',
-                   type=bool,
-                   default=False,
-                   help='True | False')
-    
-    p.add_argument('--steps',
-                   type=int,
-                   default=15,
-                   help='number of steps')
-    return p.parse_args()
+def loss_function():
+    logits = model(data, training=True)
+    return tf.losses.sparse_softmax_cross_entropy(target, logits)
 
 
-def all_reduce_benchmark(sizes, dtype=tf.float32, method='CPU', adapt=False, bench_steps = 15):
-    rank = _rank(method)
+def log(s, nl=True):
+    from kungfu.tensorflow.ops import current_rank
+    if current_rank() != 0:
+        return
+    print(s, end='\n' if nl else '')
 
-    def log(msg):
-        if rank == 0:
-            print(msg)
 
-    xs = [tf.Variable(tf.ones([n], dtype)) for n in sizes]
-    tot_size = sum(_tensor_size(x) for x in xs)
-    np = get_cluster_size(method)
-    multiplier = 4 * (np - 1)
-    log('all reduce %d tensors of total size: %s among %d peers, using %s' %
-        (len(sizes), show_size(tot_size), np, method))
+log('Model: %s' % args.model)
+log('Batch size: %d' % args.batch_size)
+device = '/gpu:0' if args.cuda else 'CPU'
 
-    # ys = _group_all_reduce_func[method](xs)
-    # ys = map_maybe(monitored_all_reduce, xs)
-    ys = [monitored_all_reduce(x, []) for x in xs]
 
-    init = tf.global_variables_initializer()
+def log_detailed_result(value, error, attrs):
+    import json
+    attr_str = json.dumps(attrs, separators=(',', ':'))
+    # grep -o RESULT.* *.log
+    print('RESULT: %f +-%f %s' % (value, error, attr_str))
 
-    warmup_steps = 5
+
+def log_final_result(value, error):
+    from kungfu.tensorflow.ops import current_rank, current_cluster_size
+    if current_rank() != 0:
+        return
+    attrs = {
+        'framework': 'kungfu',
+        'np': current_cluster_size(),
+        'strategy': os.getenv('KUNGFU_ALLREDUCE_STRATEGY'),
+        'bs': args.batch_size,
+        'model': args.model,
+        'kf-opt': args.kf_optimizer,
+        'fuse': args.fuse,
+    }
+    log_detailed_result(value, error, attrs)
+
+
+def run(benchmark_step):
+    # Warm-up
+    log('Running warmup...')
+    for x in range(args.num_warmup_batches):
+        time = timeit.timeit(benchmark_step, number=1)
+        img_sec = args.batch_size / time
+        log('Warmup Step #%d: %.1f img/sec per %s, took %.3fs' %
+            (x, img_sec, device, time))
+
+    log_stats(-1)
     changed = False
-
-    with tf.Session(config=_config(method)) as sess:
-        duration, _ = measure(lambda: sess.run(init))
-        log('tensorflow init took %.fs' % (duration))
-
-        for step in one_based_range(warmup_steps):
-            duration, _ = measure(lambda: sess.run(ys))
-            log('warmup step %d, took %.2fs, equivalent data rate: %s' %
-                (step, duration, show_rate(tot_size * multiplier, duration)))
-
-        log_stats(-1)
-        for step in one_based_range(bench_steps):
-            duration, _ = measure(lambda: sess.run(ys))
-            log('step %d, took %.2fs, equivalent data rate: %s' %
-                (step, duration, show_rate(tot_size * multiplier, duration)))
+    # Benchmark
+    log('Running benchmark...')
+    img_secs = []
+    for x in range(args.num_iters): 
+        time=0
+        for y in range(args.num_batches_per_iter):
+            time += timeit.timeit(benchmark_step, number=1)
             log_stats(default_strategy_master)
 
-            if adapt:
+            if args.adapt:
                 if changed:
                     continue
                 ret = change_strategy()
                 if ret == 1:
                     changed = True
-    if rank == 0:
+        img_sec = args.batch_size * args.num_batches_per_iter / time
+        log('Iter #%d: %.1f img/sec per %s' % (x, img_sec, device))
+        img_secs.append(img_sec)
+    
+    if current_rank() == 0:
         print_strategy_stats()
 
-
-def main(_):
-    args = parse_args()
-    if args.method == 'HOROVOD':
-        hvd_init()
-    dtype = tf.float32
-    sizes = _model_sizes[args.model]
-    if args.fuse:
-        sizes = [sum(sizes)]
-    all_reduce_benchmark(sizes, dtype, args.method, args.adapt, args.steps)
+    # Results
+    img_sec_mean = np.mean(img_secs)
+    img_sec_conf = 1.96 * np.std(img_secs)
+    log('Img/sec per %s: %.1f +-%.1f' % (device, img_sec_mean, img_sec_conf))
+    log_final_result(img_sec_mean, img_sec_conf)
 
 
-if __name__ == "__main__":
-    main(sys.argv)
+loss = loss_function()
+train_opt = opt.minimize(loss)
+
+if tf.executing_eagerly():
+    with tf.device(device):
+        run(lambda: opt.minimize(loss_function,
+                                 var_list=model.trainable_variables))
+else:
+    init = tf.global_variables_initializer()
+    bcast_op = None
+    if args.kf_optimizer:
+        from kungfu.tensorflow.initializer import BroadcastGlobalVariablesOp
+        bcast_op = BroadcastGlobalVariablesOp()
+    with tf.Session(config=config) as session:
+        from kungfu._utils import measure
+        duration, _ = measure(lambda: session.run(init))
+        log('init took %.3fs' % (duration))
+        if bcast_op:
+            duration, _ = measure(lambda: session.run(bcast_op))
+            log('bcast_op took %.3fs' % (duration))
+        run(lambda: session.run(train_opt))
+        if barrier_op is not None:
+            session.run(barrier_op)
---
+++ experimental/adapt_strategy/adapt_strategy_dev.py
@@ -0,0 +1,164 @@
+"""
+Usage:
+    kungfu-run -q -np 4 python3 -m kungfu.tensorflow.v1.benchmarks --method CPU
+    kungfu-run -q -np 4 python3 -m kungfu.tensorflow.v1.benchmarks --method NCCL
+    mpirun -np 4 python3 -m kungfu.tensorflow.v1.benchmarks --method HOROVOD
+"""
+
+import argparse
+import sys
+
+import tensorflow as tf
+from kungfu._utils import measure, one_based_range, map_maybe
+from kungfu.ext import _get_cuda_index, change_strategy, log_stats, print_strategy_stats
+from kungfu.tensorflow.ops import (current_cluster_size, current_rank,
+                                   group_all_reduce, group_nccl_all_reduce,
+                                   monitored_all_reduce)
+from kungfu.tensorflow.v1.benchmarks import model_sizes
+from kungfu.tensorflow.v1.helpers.utils import show_rate, show_size
+from tensorflow.python.util import deprecation
+
+deprecation._PRINT_DEPRECATION_WARNINGS = False
+
+default_strategy_master = 0
+
+def _tensor_size(t):
+    return t.shape.num_elements() * t.dtype.size
+
+
+def hvd_init():
+    import horovod.tensorflow as hvd
+    hvd.init()
+
+
+def hvd_group_all_reduce(ts):
+    import horovod.tensorflow as hvd
+    return [hvd.allreduce(t, average=False) for t in ts]
+
+
+def get_cluster_size(method):
+    if method == 'HOROVOD':
+        import horovod.tensorflow as hvd
+        return hvd.size()
+    else:
+        return current_cluster_size()
+
+
+_group_all_reduce_func = {
+    'CPU': group_all_reduce,
+    'NCCL': group_nccl_all_reduce,
+    'HOROVOD': hvd_group_all_reduce,
+}
+
+_model_sizes = {
+    'ResNet50': model_sizes.resnet50_imagenet,
+    'VGG16': model_sizes.vgg16_imagenet,
+    'BERT': model_sizes.bert,
+}
+
+
+def _config(method):
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    if method == 'HOROVOD':
+        import horovod.tensorflow as hvd
+        config.gpu_options.visible_device_list = str(hvd.local_rank())
+    else:
+        config.gpu_options.visible_device_list = str(_get_cuda_index())
+    return config
+
+
+def _rank(method):
+    if method == 'HOROVOD':
+        import horovod.tensorflow as hvd
+        return hvd.rank()
+    else:
+        return current_rank()
+
+def parse_args():
+    p = argparse.ArgumentParser(description='Perf Benchmarks.')
+    p.add_argument('--model',
+                   type=str,
+                   default='ResNet50',
+                   help='ResNet50 | VGG16 | BERT')
+
+    p.add_argument('--method',
+                   type=str,
+                   default='CPU',
+                   help='CPU | NCCL | HOROVOD')
+    p.add_argument('--fuse', action='store_true', default=False, help='')
+
+    p.add_argument('--adapt',
+                   type=bool,
+                   default=False,
+                   help='True | False')
+    
+    p.add_argument('--steps',
+                   type=int,
+                   default=15,
+                   help='number of steps')
+    return p.parse_args()
+
+
+def all_reduce_benchmark(sizes, dtype=tf.float32, method='CPU', adapt=False, bench_steps = 15):
+    rank = _rank(method)
+
+    def log(msg):
+        if rank == 0:
+            print(msg)
+
+    xs = [tf.Variable(tf.ones([n], dtype)) for n in sizes]
+    tot_size = sum(_tensor_size(x) for x in xs)
+    np = get_cluster_size(method)
+    multiplier = 4 * (np - 1)
+    log('all reduce %d tensors of total size: %s among %d peers, using %s' %
+        (len(sizes), show_size(tot_size), np, method))
+
+    # ys = _group_all_reduce_func[method](xs)
+    # ys = map_maybe(monitored_all_reduce, xs)
+    ys = [monitored_all_reduce(x, []) for x in xs]
+
+    init = tf.global_variables_initializer()
+
+    warmup_steps = 5
+    changed = False
+
+    with tf.Session(config=_config(method)) as sess:
+        duration, _ = measure(lambda: sess.run(init))
+        log('tensorflow init took %.fs' % (duration))
+
+        for step in one_based_range(warmup_steps):
+            duration, _ = measure(lambda: sess.run(ys))
+            log('warmup step %d, took %.2fs, equivalent data rate: %s' %
+                (step, duration, show_rate(tot_size * multiplier, duration)))
+
+        log_stats(-1)
+        for step in one_based_range(bench_steps):
+            duration, _ = measure(lambda: sess.run(ys))
+            log('step %d, took %.2fs, equivalent data rate: %s' %
+                (step, duration, show_rate(tot_size * multiplier, duration)))
+            log_stats(default_strategy_master)
+
+            if adapt:
+                if changed:
+                    continue
+                ret = change_strategy()
+                if ret == 1:
+                    changed = True
+    if rank == 0:
+        print_strategy_stats()
+
+
+def main(_):
+    args = parse_args()
+    if args.method == 'HOROVOD':
+        hvd_init()
+    dtype = tf.float32
+    sizes = _model_sizes[args.model]
+    if args.fuse:
+        sizes = [sum(sizes)]
+    all_reduce_benchmark(sizes, dtype, args.method, args.adapt, args.steps)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
