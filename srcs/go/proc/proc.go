05e14b8432c0b27c44038fe4b88ab8cdf2bc6582
lg <lg4869@outlook.com>
2020-05-30 15:09:30 +0100

    use value instead of pointer

--- srcs/go/proc/proc.go
+++ srcs/go/proc/proc.go
@@ -36,23 +36,21 @@ type Proc struct {
 	Envs     Envs
 	Hostname string
 	LogDir   string
-	ChDir    *string
+	Dir      string
 }
 
 func (p Proc) CmdCtx(ctx context.Context) *exec.Cmd {
 	cmd := exec.CommandContext(ctx, p.Prog, p.Args...)
 	cmd.Env = updatedEnv(p.Envs)
-	if p.ChDir != nil {
-		cmd.Dir = *p.ChDir
-	}
+	cmd.Dir = p.Dir
 	return cmd
 }
 
 func (p Proc) Script() string {
 	buf := &bytes.Buffer{}
 	var chdir string
-	if p.ChDir != nil {
-		chdir = fmt.Sprintf("-C %s", *p.ChDir)
+	if len(p.Dir) > 0 {
+		chdir = fmt.Sprintf("-C %s", p.Dir)
 	}
 	fmt.Fprintf(buf, "env %s\\\n", chdir)
 	for k, v := range p.Envs {
