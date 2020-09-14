c9852d524970bc8bed988e39beb21298aaba2339
Marcel Wagenländer <marwage@users.noreply.github.com>
2020-09-14 13:42:19 +0200

    Pytorch allgather support (#320)
    * checkpoint; compiling but not working
    
    * finish allgather implementation for pytorch
    
    * add a simple test
    
    * fix shape (#321)
    
    * test_device
    
    * remove unnecessary call
    
    * fix wrong variable
    
    Co-authored-by: lg <lg4869@outlook.com>
    Co-authored-by: LG <lgarithm@users.noreply.github.com>

--- srcs/cpp/src/torch/ops/cpu/collective.cpp
+++ srcs/cpp/src/torch/ops/cpu/collective.cpp
@@ -33,4 +33,31 @@ void all_reduce_cpu(torch::Tensor input, torch::Tensor output,
         std::cerr << __func__ << " not implemented for " << type << std::endl;
     }
 }
+
+template <typename T>
+void do_all_gather(void *input, void *output, size_t n)
+{
+    T *x = reinterpret_cast<T *>(input);
+    T *y = reinterpret_cast<T *>(output);
+    _default_peer->AllGather(x, n, kungfu::type_encoder::value<T>(), y, "");
+}
+
+void do_all_gather(torch::Tensor &input, torch::Tensor &output)
+{
+    using T           = float;
+    TensorShape shape = get_tensor_shape(input);
+    do_all_gather<T>(input.data_ptr(), output.data_ptr(), shape.size());
+}
+
+void all_gather_cpu(torch::Tensor input, torch::Tensor output,
+                    const std::string &type)
+{
+    const auto tt     = _torch_tensor_types.at(type);
+    TensorShape shape = get_tensor_shape(input);
+    if (tt == Torch_Cpu_Float) {
+        do_all_gather(input, output);
+    } else {
+        std::cerr << __func__ << " not implemented for " << type << std::endl;
+    }
+}
 }  // namespace kungfu
