e8327b5a21253018efc7511ba2279a67bb03ca10
lg <lg4869@outlook.com>
2020-09-18 18:08:25 +0000

    remove order_group

--- srcs/cpp/include/kungfu.h
+++ srcs/cpp/include/kungfu.h
@@ -8,13 +8,6 @@
 extern "C" {
 #endif
 
-typedef struct order_group_s order_group_t;
-
-extern order_group_t *new_ranked_order_group(int n_names);
-extern void del_order_group(order_group_t *);
-extern void order_group_do_rank(order_group_t *, int rank, callback_t *task);
-extern void order_group_wait(order_group_t *, int32_t *arrive_order);
-
 extern void kungfu_run_main();
 
 #ifdef __cplusplus
