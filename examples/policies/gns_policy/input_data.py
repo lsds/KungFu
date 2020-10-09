ddf53c44a7235e3abf950fd8494626eecdb645ca
lg <lg4869@outlook.com>
2020-10-09 11:57:03 +0000

    fix

--- examples/policies/gns_policy/input_data.py
+++ examples/policies/gns_policy/input_data.py
@@ -8,19 +8,6 @@ image_shape = {
 }
 
 
-def random_input(sample_shape=None, feature_shape=None):
-    if sample_shape is None:
-        sample_shape = image_shape['CIFAR10']
-    if feature_shape is None:
-        feature_shape = []
-    samples = tf.random_uniform(sample_shape)
-    labels = tf.random_uniform(feature_shape,
-                               minval=0,
-                               maxval=999,
-                               dtype=tf.int64)
-    return samples, labels
-
-
 def batched_random_input(batch_size, sample_shape=None, feature_shape=None):
     if sample_shape is None:
         sample_shape = image_shape['CIFAR10']
@@ -30,24 +17,9 @@ def batched_random_input(batch_size, sample_shape=None, feature_shape=None):
     labels = tf.random_uniform([batch_size] + feature_shape,
                                minval=0,
                                maxval=999,
-                               dtype=tf.int64)
+                               dtype=tf.int32)
 
     return samples, labels
-
-
-def random_dataset(epoch_size, init_batch_size=32):
-    samples, labels = random_input()
-    samples = tf.data.Dataset.from_tensors(samples)
-    labels = tf.data.Dataset.from_tensors(labels)
-    features = {'x': samples}
-    ds = tf.data.Dataset.zip((features, labels))
-    ds = ds.repeat(epoch_size)
-    batch_size = tf.Variable(init_batch_size,
-                             dtype=tf.int64,
-                             name='batch_size',
-                             trainable=False)
-    ds = ds.batch(batch_size)
-    return ds, batch_size
 
 
 def build_random_input_fn(init_batch_size, steps=None):
@@ -75,7 +47,7 @@ def build_cifar10_input_fn(init_batch_size=32):
         ds = tf.data.Dataset.zip((features, labels))
         ds = ds.repeat()
         batch_size = kf.get_or_create_batch_size(init_batch_size)
-        ds = ds.batch(batch_size)
+        ds = ds.batch(tf.cast(batch_size, tf.int64))
         return ds
 
     return input_fn
