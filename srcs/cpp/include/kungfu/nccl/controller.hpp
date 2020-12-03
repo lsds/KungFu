d2c5c6e8aaee8d2f36ef1e20d025cffcd8323e66
lg <lg4869@outlook.com>
2020-12-03 17:49:08 +0000

    NCCLController::InitOnce

--- srcs/cpp/include/kungfu/nccl/controller.hpp
+++ srcs/cpp/include/kungfu/nccl/controller.hpp
@@ -14,10 +14,14 @@ class NCCLController
     KungFu_NCCLScope scope_;
     std::unique_ptr<gpu_collective> gpu_collective_;
 
+    gpu_collective *new_gpu_collective(Peer *peer);
+
   public:
     NCCLController(const KungFu_NCCLScope scope);
 
     void InitOnce(Peer *peer);
+
+    void ReInit(Peer *peer);
 
     int Reduce(const Workspace &w, KungFu_Op op, DoneCallback done);
 
--- srcs/cpp/src/nccl/controller.cpp
+++ srcs/cpp/src/nccl/controller.cpp
@@ -40,15 +40,25 @@ void CrossAllReduceGpu(Peer *peer, const Workspace &w, KungFu_Op op,
 
 NCCLController::NCCLController(const KungFu_NCCLScope scope) : scope_(scope) {}
 
+gpu_collective *NCCLController::new_gpu_collective(Peer *peer)
+{
+    if (scope_ == KungFu_NCCL_LOCAL) {
+        return new_local_gpu_collective(*peer);
+    } else {
+        return new_global_gpu_collective(*peer);
+    }
+}
+
 void NCCLController::InitOnce(Peer *peer)
 {
     if (gpu_collective_.get() == nullptr) {
-        if (scope_ == KungFu_NCCL_LOCAL) {
-            gpu_collective_.reset(new_local_gpu_collective(*peer));
-        } else {
-            gpu_collective_.reset(new_global_gpu_collective(*peer));
-        }
+        gpu_collective_.reset(new_gpu_collective(peer));
     }
+}
+
+void NCCLController::ReInit(Peer *peer)
+{
+    gpu_collective_.reset(new_gpu_collective(peer));
 }
 
 int NCCLController::Reduce(const Workspace &w, KungFu_Op op, DoneCallback done)
